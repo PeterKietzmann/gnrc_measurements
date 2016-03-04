f0ee9d0ecd13f2ae33de583e53f60995d5e836c8
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2016-03-04 15:41:33 +0100

    adapt script to get memory allocation

--- py_save_all_txt/run_ip_and_or_udp_MEMORY.py
+++ py_save_all_txt/run_ip_and_or_udp_MEMORY.py
@@ -2,6 +2,8 @@
 
 import os, signal, sys, subprocess, serial, time
 from pexpect import spawn, TIMEOUT, EOF
+import numpy as np
+import argparse
 
 ###################### SET PARAMETERTS ######################
 
@@ -15,8 +17,8 @@ BOARD='iotlab-m3';
 #BOARD='stm32f4discovery';
 
 
-NAME_STRING_ADD = '_V2';
-#NAME_STRING_ADD = '';
+#NAME_STRING_ADD = '_V2';
+NAME_STRING_ADD = '';
 
 
 if BOARD == 'native':
@@ -41,7 +43,18 @@ path_vec.append(os.getcwd()+'/plain_udp');
 path_vec.append(os.getcwd()+'/posix_ip');
 path_vec.append(os.getcwd()+'/gnrc_conn_ip');
 path_vec.append(os.getcwd()+'/plain_ip');
+
+path_vec_elf = [];
+path_vec_elf.append(os.getcwd()+'/posix_udp/bin/'+BOARD+'/posix_udp.elf');
+path_vec_elf.append(os.getcwd()+'/gnrc_conn_udp/bin/'+BOARD+'/gnrc_conn_udp.elf');
+path_vec_elf.append(os.getcwd()+'/plain_udp/bin/'+BOARD+'/plain_udp.elf');
+path_vec_elf.append(os.getcwd()+'/posix_ip/bin/'+BOARD+'/posix_ip.elf');
+path_vec_elf.append(os.getcwd()+'/gnrc_conn_ip/bin/'+BOARD+'/gnrc_conn_ip.elf');
+path_vec_elf.append(os.getcwd()+'/plain_ip/bin/'+BOARD+'/plain_ip.elf');
+
 os.chdir("py_save_all_txt/")
+
+
 
 text_files = [
 			open('posix_udp_l2_mem_'+BOARD+NAME_STRING_ADD+'.txt', "a"), 
@@ -78,19 +91,31 @@ if BOARD == 'native':
 in_run = False
 
 loopback_mode_vec = [0, 1]#, 1]; # 0 = l2 loopback 1: ipv6 loopback 
+loopback_mode_vec_str=['L2 Reflector', 'IP Loopback'];
 
-
-payload_vec = np.arange(10, 31, 10)
+payload_vec = np.arange(10, 1211, 10);
+print str(payload_vec)
 
 number_of_measurements = len(loopback_mode_vec) * len(path_vec) * len(payload_vec)
 measurements_counter = 0;
+
+
+# Define some command line args
+p = argparse.ArgumentParser()
+p.add_argument("appdir", default="../RIOT/examples/hello-world", nargs="?", help="Full path to application dir")
+p.add_argument("board", default="iotlab-m3", nargs="?", help="BOARD to analyze")
+p.add_argument("-p", default="", help="Toolchain prefix, e.g. arm-none-eabi-")
+p.add_argument("-v", action="store_true", help="Dump symbol sizes to STDIO")
+args = p.parse_args()
+
+count=0;
 
 for k in range(0,len(loopback_mode_vec)): # loopback modes ip and l2
 	for y in range(0,len(payload_vec)):
 
 		# Make a copy of the environment and modify that.
 		myenv = dict(os.environ) 
-		myenv["CFLAGS"] = ('-DPACKET_SIZE='+str(payload_vec(y))+' -DLOOPBACK_MODE='+str(loopback_mode_vec[k]) ' ')
+		myenv["CFLAGS"] = ('-DPACKET_SIZE='+str(payload_vec[y])+' -DLOOPBACK_MODE='+str(loopback_mode_vec[k]));
 
 		myenv["BOARD"] = BOARD
 
@@ -106,55 +131,28 @@ for k in range(0,len(loopback_mode_vec)): # loopback modes ip and l2
 			subprocess.call(['rm -rf bin/'], shell=True)
 
 			# Pass the modified environment to the subprocess.
-			subprocess.check_call(["make"], env=myenv)
+			myproc = subprocess.check_call(["make"], env=myenv)
+			# print 'len(path_vec): '+str(len(text_files))+' index: '+str(x)
+			# print 'len(text_files): '+str(len(path_vec))+' index: '+str((k*len(path_vec))+x)
+			# print 'len(payload_vec): '+str(len(payload_vec))+' index: '+str(y)
+			# print 'len(path_vec_elf): '+str(len(path_vec_elf))+' index: '+str(x)
 
-			if BOARD == 'native':
-				myproc = subprocess.Popen(native_elf_vec[x], stdout=subprocess.PIPE)
+			myproc = subprocess.Popen((args.p + 'size', path_vec_elf[x]), stdout=subprocess.PIPE)
 
-			else:
-				# Flash to MCU by interactiong with shell
-				subprocess.call(['make flash'], env=myenv, shell=True)
+			debug_string = 'Path: '+path_vec[x]+' ' +loopback_mode_vec_str[k]+' Board: '+BOARD+' Payload: '+str(payload_vec[y])
+			print debug_string+'\n'
+			
+			#text_files[(k*len(path_vec))+x].write(debug_string+'\n')
 
-				string = print_loopback_mode[k]+' '+payload_vec(y)+' '+path_vec[x]+'\n'
-				print string
+			c = myproc.stdout.readline()
 
-			while(1):
-				if BOARD != 'native':
-					c = port.readline().strip()
-					print c
-				else:
-					c = myproc.stdout.readline().strip()
+			# just the second line is relevant
+			c = myproc.stdout.read(40)
+			text_files[(k*len(path_vec))+x].write(c+'\n')
 
-				if c != 'DONE' and in_run:
-					#print(c)
-					#text_files[(y +(k*3))+switch_API_udp_ip].write(c+'\n')
-					text_files[(k*len(path_vec))+x].write(c+'\n')
 
-				if c == 'START':
-					print 'START identified'
-					in_run = True
-					#string = print_loopback_mode[k]+' '+print_mean_mode[y]+' '+path_vec[x]+'\n'
-					#text_files[(y +(k*3))+switch_API_udp_ip].write(string) #
+			###print c
 
-				if c == 'DONE':
-					print 'DONE identified'
-					in_run = False
-					#text_files[(y +(k*3))+switch_API_udp_ip].write('\n')
-					text_files[(k*len(path_vec))+x].write(c+'\n')
-					break
-
-				# else:
-				# 	print c
-				# 	text_files[(y +(k*3))+switch_API_udp_ip].write(c+'\n')
-
-			# !in_run
-			if BOARD == 'native':
-				myproc.kill()
-
-			time.sleep(1)
-
-if BOARD != 'native':
-	port.close();    # Just call this in the last iteration!
 
 for a in range (0, len(text_files)):
 	text_files[a].close()
